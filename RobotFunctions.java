e413c184a9b2103bbcc0909395cb4ff3c9516b2a
kaavyad25 <kaavyadev25@gmail.com>
2025-11-04 16:24:15 -0700

    Fixed Outtake method

--- RobotFunctions.java
+++ RobotFunctions.java
@@ -1,55 +1,82 @@
 package org.firstinspires.ftc.teamcode;
 
+
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
 public class RobotFunctions {
 
-    DcMotor leftIntakeMotor;
-    DcMotor rightIntakeMotor;
+    DcMotor leftIntakeMotor, rightIntakeMotor, leftOuttakeMotor, rightOuttakeMotor;
 
-    DcMotor leftOuttakeMotor;
-    DcMotor rightOuttakeMotor;
+    Servo launchServo;
 
 
     public void init(HardwareMap hwMap){
 
-        leftIntakeMotor = hwMap.get(DcMotor.class, "leftIntakeMotor");
-        rightIntakeMotor = hwMap.get(DcMotor.class, "rightIntakeMotor");
+        this.leftIntakeMotor = hwMap.get(DcMotor.class, "leftIntakeMotor");
+        this.rightIntakeMotor = hwMap.get(DcMotor.class, "rightIntakeMotor");
 
-        leftOuttakeMotor = hwMap.get(DcMotor.class, "leftOuttakeMotor");
-        rightOuttakeMotor = hwMap.get(DcMotor.class, "rightOuttakeMotor");
+        this.leftOuttakeMotor = hwMap.get(DcMotor.class, "leftOuttakeMotor");
+        this.rightOuttakeMotor = hwMap.get(DcMotor.class, "rightOuttakeMotor");
 
-        leftIntakeMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        rightIntakeMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        this.launchServo = hwMap.get(Servo.class, "launchServo");
 
-        leftOuttakeMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        rightOuttakeMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        this.leftIntakeMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        this.rightIntakeMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-        leftIntakeMotor.setDirection(DcMotor.Direction.FORWARD);
-        rightIntakeMotor.setDirection(DcMotor.Direction.REVERSE);
+        this.leftOuttakeMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        this.rightOuttakeMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-        leftOuttakeMotor.setDirection(DcMotor.Direction.FORWARD);
-        rightOuttakeMotor.setDirection(DcMotor.Direction.REVERSE);
+        this.leftIntakeMotor.setDirection(DcMotor.Direction.FORWARD);
+        this.rightIntakeMotor.setDirection(DcMotor.Direction.REVERSE);
+
+        this.leftOuttakeMotor.setDirection(DcMotor.Direction.FORWARD);
+        this.rightOuttakeMotor.setDirection(DcMotor.Direction.REVERSE);
 
     }
 
-    public void activateIntake(double power, boolean isPressed) {
+    public void activateIntake(double power, boolean isHeld) {
 
-        if (isPressed) {
-            leftIntakeMotor.setPower(power);
-            rightIntakeMotor.setPower(power);
+        if (isHeld) {
+            this.leftIntakeMotor.setPower(power);
+            this.rightIntakeMotor.setPower(power);
+        } else {
+            this.leftIntakeMotor.setPower(0.0);
+            this.rightIntakeMotor.setPower(0.0);
         }
+
+    }
+    public final void halfway(){ this.launchServo.setPosition(0.5); }
+    public final void open(){ this.launchServo.setPosition(1.0); }
+    public final void close(){ this.launchServo.setPosition(0.0);}
+
+    public void centerFromLeft(double power, boolean isHeld) {
+        if (isHeld){ this.leftIntakeMotor.setPower(power);}
+        else {this.leftIntakeMotor.setPower(0.0);}
     }
 
-    public void activateOuttake(double power, boolean isPressed) {
+    public void centerFromRight(double power, boolean isHeld) {
 
-        if (isPressed) {
+        if (isHeld) {this.rightIntakeMotor.setPower(power); }
+        else { this.rightIntakeMotor.setPower(0.0); }
+    }
+    public void activateOuttake(double power) {
 
-            leftOuttakeMotor.setPower(power);
-            rightOuttakeMotor.setPower(power);
-        }
 
+        this.leftOuttakeMotor.setPower(power);
+        this.rightOuttakeMotor.setPower(power);
+
+        this.open();
+
+    }
+
+    public final void resetOuttake(){
+
+        this.leftOuttakeMotor.setPower(0.0);
+        this.rightOuttakeMotor.setPower(0.0);
+
+        this.close();
     }
 
 }
